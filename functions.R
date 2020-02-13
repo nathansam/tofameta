23c50ddad1add9589a1b9c3d1ad66b59c627049b
Nathan <ncooke96@gmail.com>
2020-02-13 11:53:01 +0000

    Create function to streamline analysis. Closes #2
    MetaAnalysis() calculates estimates and predictions of tau^2, I^2 and proportions as well as their confidence intervals. Also generates forest plots.

---
+++ functions.R
@@ -0,0 +1,70 @@
+MetaAnalysis <- function(cases, total, authorYear, data, xlab, dir){
+  
+  if(dir.exists(dir) == F) dir.create(dir)
+  
+  outcome <- deparse(substitute(cases))
+  
+  cases <- round(eval(substitute(cases), data, parent.frame()))
+  total <- round(eval(substitute(total), data, parent.frame()))
+  authorYear <- eval(substitute(authorYear), data, parent.frame())
+
+  # Logit transfromation
+  ies <- metafor::escalc(xi = cases, ni = total, measure = "PLO")
+  # Random effects model fitted using the DerSimonian and Laird
+  pes.logit <- metafor::rma(yi, vi, data = ies, method = "DL")
+  
+  
+  values <- confint(pes.logit)$random
+  tau2 <- matrix(values[1, ], nrow = 1) # tau^2 pred + CI values
+  colnames(tau2) <- c("tau2", "tau2.lb", "tau2.up")
+  
+  I2 <- matrix(values[3, ], nrow = 1) # I^2 pred + CI values
+  colnames(I2) <- c("I2", "I2.lb", "I2.up")
+  
+  
+  # Inverse logit transformation
+  pes <- predict(pes.logit, transf = transf.ilogit)
+  
+  # Predicted proportion + CI values
+  pred <- matrix(c(pes$pred, pes$ci.lb, pes$ci.ub), nrow = 1)
+  colnames(pred) <- c("prop", "prop.lb", "prop.ub")
+  
+  result <- cbind(tau2, I2, pred)
+  rownames(result) <- outcome
+  
+  #### Plots ####
+  
+  pes.summary <- meta::metaprop(round(cases), total, authorYear,
+                                sm = "PLO", method.tau="DL", method.ci="NAsm")
+  
+  
+  png(file = paste(dir, "/", outcome, ".png", sep = ""), width = 600)
+  
+  meta::forest(pes.summary,
+               rightcols = FALSE,
+               leftcols = c("studlab", "event", "n", "effect", "ci"),
+               leftlabs = c("Study", "Cases", "Total", "Proportion",
+                            "95% C.I."),
+               xlab = xlab, smlab = "", weight.study = "random",
+               squaresize = 0.5, col.square = "navy", col.square.lines = "navy",
+               col.diamond = "maroon", col.diamond.lines = "maroon",
+               pooled.totals = FALSE, comb.fixed = FALSE, fs.hetstat = 10,
+               print.tau2 = TRUE, print.Q = TRUE, print.pval.Q = TRUE,
+               print.I2 = TRUE, digits = 2)
+  dev.off()
+  
+  svg(file = paste(dir, "/", outcome, ".svg", sep = ""), width = 8)
+  
+  meta::forest(pes.summary, rightcols = FALSE,
+               leftcols = c("studlab", "event", "n", "effect", "ci"),
+               leftlabs = c("Study", "Cases", "Total", "Proportion",
+                            "95% C.I."),
+               xlab = xlab, smlab = "", weight.study = "random",
+               squaresize = 0.5, col.square = "navy", col.square.lines = "navy",
+               col.diamond = "maroon", col.diamond.lines = "maroon",
+               pooled.totals = FALSE, comb.fixed = FALSE, fs.hetstat = 10,
+               print.tau2 = TRUE, print.Q = TRUE, print.pval.Q = TRUE,
+               print.I2 = TRUE, digits=2)
+  dev.off()
+  return(result)
+}
