37662d45a7e69230ef449b86301b5b543256fac4
Nathan <ncooke96@gmail.com>
2020-06-08 18:27:02 +0100

    Add influential study indicators & reformat

--- functions.R
+++ functions.R
@@ -1,7 +1,10 @@
 ################################################################################
-### R script for  the MetaAnalysis function used in Lucaciu et al.'s pooled  ###
-### analysis of tofacitinib efficacy & safety. Script written by Nathan      ###
-### Constantine-Cooke https://github.com/nathansam                           ###
+### R script for  the MetaAnalysis function used in Lucaciu &                ###
+### Constantine-Cooke et al.'s                                               ###
+### "Real-world experience with tofacitinib in ulcerative colitis -          ###
+### a systematic review and  meta-analysis"                                  ###
+### Script written by Nathan Constantine-Cooke                               ###  
+### https://github.com/nathansam                                             ###
 ################################################################################
 
 MetaAnalysis <- function(cases, total, authorYear, data, xlab, dir,
@@ -13,8 +16,12 @@ MetaAnalysis <- function(cases, total, authorYear, data, xlab, dir,
   outcome <- deparse(substitute(cases)) # used for row name of result
   
   cases <- round(eval(substitute(cases), data, parent.frame()))
+  non.NA <- is.na(cases) == F
+  cases <- cases[non.NA]
   total <- round(eval(substitute(total), data, parent.frame()))
+  total <- total[non.NA]
   authorYear <- eval(substitute(authorYear), data, parent.frame())
+  authorYear <- authorYear[non.NA]
   
   # Logit transfromation
   ies <- metafor::escalc(xi = cases, ni = total, measure = "PLO")
@@ -35,6 +42,10 @@ MetaAnalysis <- function(cases, total, authorYear, data, xlab, dir,
   I2 <- matrix(values[3, ], nrow = 1) # I^2 pred + CI values
   colnames(I2) <- c("I2", "I2.lb", "I2.up")
   
+  # * for influential studies
+  influential <- inf$inf$inf
+  authorYear <- paste(authorYear, influential)
+  
   # Inverse logit transformation
   pes <- predict(pes.logit, transf = transf.ilogit)
   
@@ -47,39 +58,72 @@ MetaAnalysis <- function(cases, total, authorYear, data, xlab, dir,
   
   #### Forest plot ####
   
-  pes.summary <- meta::metaprop(round(cases), total, authorYear,
-                                sm = "PLO", method.tau="DL", method.ci="NAsm")
+  pes.summary <- meta::metaprop(round(cases),
+                                total,
+                                authorYear,
+                                sm = "PLO",
+                                method.tau="DL",
+                                method.ci="NAsm")
   
   # xlim larger than maximum CI value and a multiple of 0.2.
-  xlim <- (max(ceiling(pes.summary$upper[is.na(pes.summary$upper) == F] / 0.2)) * 0.2) 
+  xlim <- (max(ceiling(pes.summary$upper[is.na(pes.summary$upper) ==
+                                           F] / 0.2)) * 0.2) 
   
   
   png(file = paste(dir, "/", outcome, ".png", sep = ""), width = 600)
   meta::forest(pes.summary,
                rightcols = FALSE,
                leftcols = c("studlab", "event", "n", "effect", "ci"),
-               leftlabs = c("Study", "Cases", "Total", "Proportion",
+               leftlabs = c("Study",
+                            "Cases",
+                            "Total",
+                            "Proportion",
                             "95% C.I."),
-               xlab = xlab, smlab = "", weight.study = "random",
-               squaresize = 0.5, col.square = "navy", col.square.lines = "navy",
-               col.diamond = "maroon", col.diamond.lines = "maroon",
-               pooled.totals = FALSE, comb.fixed = FALSE, fs.hetstat = 12,
-               print.tau2 = TRUE, print.Q = TRUE, print.pval.Q = TRUE,
-               print.I2 = TRUE, digits = 2, xlim = c(0, xlim))
+               xlab = xlab,
+               smlab = "",
+               weight.study = "random",
+               squaresize = 0.5,
+               col.square = "navy",
+               col.square.lines = "navy",
+               col.diamond = "maroon",
+               col.diamond.lines = "maroon",
+               pooled.totals = FALSE,
+               comb.fixed = FALSE,
+               fs.hetstat = 12,
+               print.tau2 = TRUE,
+               print.Q = TRUE,
+               print.pval.Q = TRUE,
+               print.I2 = TRUE,
+               digits = 2,
+               xlim = c(0, xlim))
   dev.off()
   
   # Same plot again but saved in vector format. 
   pdf(file = paste(dir, "/", outcome, ".pdf", sep = ""), width = 8)
   meta::forest(pes.summary, rightcols = FALSE,
                leftcols = c("studlab", "event", "n", "effect", "ci"),
-               leftlabs = c("Study", "Cases", "Total", "Proportion",
+               leftlabs = c("Study",
+                            "Cases",
+                            "Total",
+                            "Proportion",
                             "95% C.I."),
-               xlab = xlab, smlab = "", weight.study = "random",
-               squaresize = 0.5, col.square = "navy", col.square.lines = "navy",
-               col.diamond = "maroon", col.diamond.lines = "maroon",
-               pooled.totals = FALSE, comb.fixed = FALSE, fs.hetstat = 12,
-               print.tau2 = TRUE, print.Q = TRUE, print.pval.Q = TRUE,
-               print.I2 = TRUE, digits = 2, xlim = c(0, xlim))
+               xlab = xlab,
+               smlab = "",
+               weight.study = "random",
+               squaresize = 0.5,
+               col.square = "navy",
+               col.square.lines = "navy",
+               col.diamond = "maroon",
+               col.diamond.lines = "maroon",
+               pooled.totals = FALSE,
+               comb.fixed = FALSE,
+               fs.hetstat = 12,
+               print.tau2 = TRUE,
+               print.Q = TRUE,
+               print.pval.Q = TRUE,
+               print.I2 = TRUE,
+               digits = 2,
+               xlim = c(0, xlim))
   dev.off()
   return(result)
 }
